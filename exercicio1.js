778666b0fca9e09dad48f1cde7921d0b1da38404
Beatriz Corrêa <bscorrea22@hotmail.com>
2025-05-21 19:03:04 -0300

    Subir os exercicios da aula

---
+++ exercicio1.js
@@ -0,0 +1,12 @@
+const nome = "Amora"
+let idade = "2 anos"
+let peso = 10.6
+const raca = "salsichinha"
+let adotado = true
+
+
+//TAG
+console.log('TAG')
+
+
+console.log('Nome:', nome.toUpperCase() + ' | Peso: ' , peso + ' | Raça: ' , raca.charAt(0).toUpperCase() +  raca.slice(1).toLowerCase()) 
---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+
+const nomes = ['Amora', 'anora', 'Teste', 'P125sa'];
+const nomesFormatados = [];
+for (let i = 0; i < nomes.length; i++) {
+  let capitalizeArray = nomes[i].toUpperCase();
+  nomesFormatados.push(capitalizeArray); 
+  };
+
+
+
+console.log('Nome Original: ' , nomes)
+console.log('Nome Formatado: ', nomesFormatados);

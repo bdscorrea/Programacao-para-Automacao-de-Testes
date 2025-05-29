07b6728c00298c23af8aabd4d70e6005c19772b0
BEATRIZ CORRÊA <66252245+bdscorrea@users.noreply.github.com>
2025-05-29 17:44:11 -0300

    exercicios em aula

---
+++ exercicio7.js
@@ -0,0 +1,20 @@
+const quantidadePetiscosDisponiveis = 4
+for (let petisco = 1; petisco <= quantidadePetiscosDisponiveis; petisco++){
+    console.log(`Dando petisco de nº ${petisco} para a Pipoca`)
+} 
+ console.log(`Satifeito!`)
+
+ 
+console.log(`---------------FUNCTION----------------------`)
+
+
+function estaSatisfeito() {
+const quantidadePetiscosDisponiveis = 4
+for (let petisco = 1; petisco <= quantidadePetiscosDisponiveis; petisco++){
+    console.log(`Dando petisco de nº ${petisco} para a Pipoca`)
+} 
+ console.log(`Satifeito!`)
+
+}
+
+estaSatisfeito()

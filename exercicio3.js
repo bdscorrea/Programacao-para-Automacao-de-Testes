39b3863264e341b01f0c56f48f7410d6c0822055
BEATRIZ CORRÊA <66252245+bdscorrea@users.noreply.github.com>
2025-05-29 13:57:29 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,12 @@
+const nome = 'Loki'
+const porte = 'pequeno' 
+const idadeDog = 0
+const adocao = idadeDog <= 3 && porte != 'pequeno'
+              ? 'Não pode adotar!'
+              : 'Pode adotar!'
+             
+
+
+console.log('Nome: ', nome)
+console.log('Idade: ', idadeDog)
+console.log(adocao)     

b2e5bc6823ca3e6aa92769faca46edb279b74d0a
BEATRIZ CORRÊA <66252245+bdscorrea@users.noreply.github.com>
2025-05-30 14:05:35 -0300

    Resolve conflitos de merge

--- modularizacao-export.js
+++ modularizacao-export.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-/**/
-
 function exibirNomePet(paramNomePet) {
     console.log(`O nome do pet é ${paramNomePet}`)
 }
@@ -17,24 +14,4 @@ function exibirIdade(paramIdadePet) {
 export {
     exibirNomePet,
     exibirIdade
-=======
-/**/
-
-function exibirNomePet(paramNomePet) {
-    console.log(`O nome do pet é ${paramNomePet}`)
-}
-
-function exibirIdade(paramIdadePet) {
-    console.log(`Idade do pet é ${paramIdadePet}`)
-}
-
-//exibirNomePet('Pipoca')
-//exibirNomePet('Thor')
-//exibirNomePet('Xuxu')
-
-
-export {
-    exibirNomePet,
-    exibirIdade
->>>>>>> 70ce6fed1308c54987bb259526e61fe5c1a8a6e3
 }
